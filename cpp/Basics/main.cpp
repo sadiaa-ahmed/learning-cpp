da3e5684f8c9e06134da19bc901060ef72d26f3d
Sadia Ahmed <sadiaahmed26373@gmail.com>
2025-04-24 17:07:02 -0700

    1st Program (Basics)

---
+++ cpp/Basics/main.cpp
@@ -0,0 +1,37 @@
+// Command For Run (Ctrl + Alt + N)
+// Command For Compile (g++ (file name) -o main ->output file name (g++ main.cpp -o main)) 
+// Command For Run(file name.exe)
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "Hey! You're running C++ in VS Code!" << endl;
+//     return 0;
+// }
+
+// *******************************************************
+// #include <iostream>
+// #include <conio.h> // Required for getch()
+
+// using namespace std;
+
+// main()
+// {
+//     int a;
+//     cout << "Please Enter 4 Digit Number: ";
+//     cin >> a;
+
+//     cout << "\n 4 Digits are: ";
+//     cout << a % 10 << ", ";
+//     a = a / 10;
+//     cout << a % 10 << ", ";
+//     a = a / 10;
+//     cout << a % 10 << ", ";
+//     a = a / 10;
+//     cout << a % 10;
+
+//     getch(); 
+//     return 0;
+// }
+
