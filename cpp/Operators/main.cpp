ce3da5d00dba439630324d34ed6b5aea80231b75
Sadia Ahmed <sadiaahmed26373@gmail.com>
2025-04-24 17:30:24 -0700

    operators

---
+++ cpp/Operators/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	// Airthmetic Operators  + = - /
+    // Assignment Operators  = += =+ /= %=
+	// Comparision Operators  == > < <= >= != 
+    // Logical AND OR NOT (&& || ! )
+
+	int amir_age , ammara_age;
+	cout << "\n Enter age of Amir: ";
+    cin >> amir_age;
+    cout << "\n Enter age of Ammara: ";
+    cin >> ammara_age;
+	if (amir_age > ammara_age)
+	{
+		cout<<"\n\n Amir is older than Ammara";
+	}else{
+		cout<<"\n\n Ammara is older than Amir";
+	}
+	return 0 ;
+}
+
+// ***********************************
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int start = 1 ;
+	while(start<= 10)
+	{
+		cout<<"\n\n Hey";
+		start++ ;
+	}
+//	int i ;
+//	for(i = 0 ; i < 3 ; i++){
+//		cout<< "\n And He Found You Lost And Guided You";
+//	}
+
+	return 0 ;
+}
+// ***************************************
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int start = 1 , sum =0 ; 
+	while (start <= 100)
+	{
+		sum += start ; 
+		start ++ ; 
+	}
+	cout<< "\n sum is :"<<sum ;
+	return 0;
+}
