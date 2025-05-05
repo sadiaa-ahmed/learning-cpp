a3f8cd39cbe02e99c9cde245b7002c49d436532a
Sadia Ahmed <sadiaahmed26373@gmail.com>
2025-05-04 21:15:48 -0700

    arrays

---
+++ cpp/Arrays/main.cpp
@@ -0,0 +1,41 @@
+// #include<iostream>
+
+// using namespace std;
+
+// main()
+// {
+//     int a[5];   // 5-element integer array
+
+//     //1st for loop – taking input from user
+//     for(int i=0; i<=4; i++)
+//     {
+//         cout<<"\n Enter value of Index " <<i<<":" ;
+//         cin>>a[i];
+//     }
+    
+//     // 2nd for loop – displaying stored values
+//     for(int i=0; i<=4; i++)
+//     {
+//         cout<<"\n Index of "<< i <<" : "<<a[i];
+//     }
+//     return 0 ;
+// }
+
+// --------------------------------------------------------------------------------------
+
+// #include<iostream>
+
+// using namespace std ;
+
+// main()
+// {
+//     string array[5] = {"ALi","maha", "khan","saad","Akbar"};
+//     for(int i=0; i<=4; i++)
+//     {
+//         cout<<"\n " <<array[i];
+//     }
+// }
+
+// -----------------------------------------------------------------------
+
+
